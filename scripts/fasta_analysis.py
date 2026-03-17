f5af02216412d850f3bf29533bd656d5c6baba59
harelb69 <harelb69@gmail.com>
2026-03-17 12:54:32 +0200

    added gc content

--- scripts/fasta_analysis.py
+++ scripts/fasta_analysis.py
@@ -6,5 +6,12 @@ def read_fasta(filename):
                 seq += line.strip()
     return seq
 
+def gc_content(seq):
+    g = seq.count("G")
+    c = seq.count("C")
+    gc = (g + c) / len(seq) * 100
+    return gc
+
 sequence = read_fasta("data/sequence.fasta")
 print(sequence)
+print("GC content:", gc_content(sequence))
